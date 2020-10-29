e8eaafcfa2670986339f546cba1cfc0bc8771bee
Dreher <julio.s.dreher@gmail.com>
2020-10-29 09:25:53 -0300

    Att program

--- CSharp/Xadrez/Xadrez_console/Xadrez_console/Program.cs
+++ CSharp/Xadrez/Xadrez_console/Xadrez_console/Program.cs
@@ -22,6 +22,11 @@ namespace Xadrez_console
 
                     Console.Write("Origem: ");
                     Posicao origem = Tela.LerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.Tab.Peca(origem).MovimentosPossiveis();
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(partida.Tab, posicoesPossiveis);
+
                     Console.Write("Destino: ");
                     Posicao destino = Tela.LerPosicaoXadrez().toPosicao();
                     partida.ExecutaMovimento(origem, destino);
